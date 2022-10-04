adcfe08a76515614c8b33960129047279dadeee3
Oleg <106033849+ytreeeeeeeeeeee@users.noreply.github.com>
2022-10-04 12:21:28 +0300

    task#1

--- index.php
+++ index.php
@@ -29,8 +29,20 @@ $user_name = 'ytre'; // укажите здесь ваше имя
         <nav class="user-menu">
             <?php if ($is_auth == 1): ?>
             <div class="user-menu__logged">
-                <p></p>
+                <p><?=$user_name ?></p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
             </div>
+            <?php else:?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
