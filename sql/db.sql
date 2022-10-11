1d67955f3bf7aa83c39629c7ade26c7437985271
kvaskov <kvaskowal@gmail.com>
2022-10-11 14:13:32 +0300

    class: добавили бд и запросы на добавление категорий

---
+++ sql/db.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE yeticaveKvSh
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_0900_ai_ci;
+
+  USE yeticaveKvSh;
+
+CREATE TABLE categories (
+  id    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title  VARCHAR(150) NOT NULL UNIQUE,
+  symbol_code VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+   id    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   reg_date  DATETIME NOT NULL,
+   email VARCHAR(100) NOT NULL UNIQUE,
+   user_name VARCHAR(50) NOT NULL,
+   password  VARCHAR(255) NOT NULL,
+   contact VARCHAR(15) NOT NULL
+);
+
+
+CREATE TABLE lots (
+  id    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date_of_creation  DATETIME NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  image_url VARCHAR(150) NOT NULL,
+  start_price DECIMAL NOT NULL,
+  end_date  DATE NOT NULL,
+  bet_step  INT NOT NULL,
+  author_id INT NOT NULL,
+  winner_id INT NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id),
+  FOREIGN KEY (category_id) REFERENCES  categories(id)
+);
+
+CREATE TABLE bets (
+  id    INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  date_bet  DATETIME NOT NULL,
+  bet_amount  DECIMAL NOT NULL,
+  user_id INT NOT NULL,
+  lot_id  INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (lot_id) REFERENCES lots(id)
+);
+
---
+++ sql/queries.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories(title, symbol_code) VALUES ("Доски и лыжи", "boards");
+INSERT INTO categories(title, symbol_code) VALUES ("Крепления", "attachment");
+INSERT INTO categories(title, symbol_code) VALUES ("Ботинки", "boots");
+INSERT INTO categories(title, symbol_code) VALUES ("Одежда", "clothing");
+INSERT INTO categories(title, symbol_code) VALUES ("Инструменты", "tools");
+INSERT INTO categories(title, symbol_code) VALUES ("Разное", "other");
+
